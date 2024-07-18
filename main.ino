078cda68a5c22c90900af1c63192b34449b43cc7
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-18 06:54:06 +0530

    macros

--- main.ino
+++ main.ino
@@ -1,3 +1,31 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <U8g2lib.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
+
+#define OLED_RESET -1       
+#define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int ref_eye_height = 40;
+int ref_eye_width = 40;
+int ref_space_between_eye = 10;
+int ref_corner_radius = 10;
+int left_eye_height = ref_eye_height;
+int left_eye_width = ref_eye_width;
+int left_eye_x = 32;
+int left_eye_y = 32;
+int right_eye_x = 32 + ref_eye_width + ref_space_between_eye;
+int right_eye_y = 32;
+int right_eye_height = ref_eye_height;
+int right_eye_width = ref_eye_width;
+
+
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
